99431467e76ff4b1f9a69cb42bd4d19ae95921ff
lost <derribado@tutanota.com>
2019-09-28 09:04:51 -0700

    Removed some unnecesary parsing

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let text: String;
+    let text;
     match args.len() {
         1 | 2 => {
             println!("Please specify an option.");
             std::process::exit(1)
         }
         3 => text = input::get_input(),
-        4 => text = args[3].parse::<String>().unwrap(),
+        4 => text = String::from(&args[3]),
         _ => { 
             println!("Please specify 3 or less options");
             std::process::exit(1);
@@ -19,7 +19,7 @@ fn main() {
     } 
 
     // TODO: replace unwrap() with some error handling
-    let mode = args[1].parse::<String>().unwrap();
+    let mode = String::from(&args[1]);
     let key = args[2].parse::<usize>().unwrap();
     
     if mode == "encrypt" {
