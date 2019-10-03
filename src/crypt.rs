b8dff111bbd39e5956a4e105d08576f47fb745c2
lost <derribado@tutanota.com>
2019-10-03 11:39:10 -0700

    Removed redundant check

--- src/crypt.rs
+++ src/crypt.rs
@@ -8,20 +8,18 @@ pub fn encrypt(plaintext: String, key: usize) -> String {
     let mut ciphertext = String::with_capacity(plaintext.capacity());
 
     for letter in plaintext.chars() {
-        if letter.is_ascii_alphabetic() {
-            let pos = ASCII_ALPHABET
-                .iter()
-                .position(|&bet| bet == letter)
-                .unwrap();
-            let new_pos = (key + pos) % 26;
-
-            if letter.is_uppercase() {
-                ciphertext.push(ASCII_ALPHABET[new_pos + 26]);
-            } else {
-                ciphertext.push(ASCII_ALPHABET[new_pos]);
+        match ASCII_ALPHABET.iter().position(|&bet| bet == letter) {
+            Some(pos) => { 
+                let new_pos = (key + pos) % 26;
+                if letter.is_uppercase() {
+                    ciphertext.push(ASCII_ALPHABET[new_pos + 26]);
+                } else {
+                    ciphertext.push(ASCII_ALPHABET[new_pos]);
+                }
             }
-        } else {
-            ciphertext.push(letter);
+            None => {
+                ciphertext.push(letter);
+            }
         }
     }
     ciphertext
@@ -31,20 +29,18 @@ pub fn decrypt(ciphertext: String, key: usize) -> String {
     let mut plaintext = String::with_capacity(ciphertext.capacity());
 
     for letter in ciphertext.chars() {
-        if letter.is_ascii_alphabetic() {
-            let pos = ASCII_ALPHABET
-                .iter()
-                .position(|&bet| bet == letter)
-                .unwrap();
-            let new_pos = ((pos as isize) - (key as isize)).rem_euclid(26);
-
-            if letter.is_uppercase() {
-                plaintext.push(ASCII_ALPHABET[new_pos as usize + 26]);
-            } else {
-                plaintext.push(ASCII_ALPHABET[new_pos as usize]);
+        match ASCII_ALPHABET.iter().position(|&bet| bet == letter) {
+            Some(pos) => { 
+                let new_pos = ((pos as isize) - (key as isize)).rem_euclid(26);
+                if letter.is_uppercase() {
+                    plaintext.push(ASCII_ALPHABET[new_pos as usize + 26]);
+                } else {
+                    plaintext.push(ASCII_ALPHABET[new_pos as usize]);
+                }
             }
-        } else {
-            plaintext.push(letter);
+            None => {
+                plaintext.push(letter);
+            }
         }
     }
     plaintext
