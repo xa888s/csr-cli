2751ee98ce53c38f0ad7a54714e167d7eed8a7f5
lost <derribado@tutanota.com>
2019-10-07 12:02:38 -0700

    Cleaned up some redundant code

--- src/threads.rs
+++ src/threads.rs
@@ -6,42 +6,33 @@ use std::thread;
 mod crypt;
 
 pub fn run_jobs(message: String, mode: String, key: u8, threads: usize) {
-    let size: usize = message.len() / threads;
-    let jobs;
+    // index of last char in String
+    let length = message.len();
+
+    let size: usize = length / threads;
 
     // if the length of the message is less than the thread count, do the job on one thread only
-    if message.len() < threads {
-        jobs = 0;
-    } else {
-        jobs = threads;
-    }
+    let jobs = if length < threads { 0 } else { threads };
 
     let main_thread_result;
     let mut children = Vec::with_capacity(jobs);
 
-    // index of last char in String
-    let length = message.len();
+    let func = match &mode[..] {
+        "encrypt" => crypt::encrypt,
+        
+        "decrypt" => crypt::decrypt,
 
-    match &mode[..] {
-        "encrypt" => {
-            for index in 0..jobs {
-                let chunk = String::from(&message[index * size..(index + 1) * size]);
-                children.push(thread::spawn(move || crypt::encrypt(chunk, key)));
-            }
-            main_thread_result = crypt::encrypt(String::from(&message[size * jobs..length]), key);
-        }
-        "decrypt" => {
-            for index in 0..jobs {
-                let chunk = String::from(&message[index * size..(index + 1) * size]);
-                children.push(thread::spawn(move || crypt::decrypt(chunk, key)));
-            }
-            main_thread_result = crypt::decrypt(String::from(&message[size * jobs..length]), key);
-        }
         _ => {
             eprintln!("Mode must be 'encrypt' or 'decrypt'");
             std::process::exit(1);
         }
+    };
+
+    for index in 0..jobs {
+        let chunk = String::from(&message[index * size..(index + 1) * size]);
+        children.push(thread::spawn(move || func(chunk, key)));
     }
+    main_thread_result = func(String::from(&message[size * jobs..length]), key);
 
     let stdout = io::stdout();
     let mut handle = stdout.lock();
