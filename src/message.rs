b5365206c27708545aaff29ea44e60368445edb7
lost <derribado@tutanota.com>
2019-10-06 19:43:34 -0700

    Refactored a bit by adding a Message struct

---
+++ src/message.rs
@@ -0,0 +1,96 @@
+pub enum Mode {
+    Encrypt,
+    Decrypt,
+}
+
+pub struct Message {
+    pub text: String,
+    pub mode: Mode,
+}
+
+impl Message {
+    pub fn new(text: String) -> Message {
+        Message {
+            text: text,
+            mode: Mode::Encrypt,
+        }
+    }
+
+    pub fn encrypt(self, key: u8) -> String {
+        let mut ciphervec: Vec<u8> = Vec::with_capacity(self.text.len());
+
+        for char in self.text.bytes() {
+            ciphervec.push(match char {
+                65..=90 => {
+                    let pos = char % 65;
+                    65 + ((pos + key) % 26)
+                }
+                97..=122 => {
+                    let pos = char % 97;
+                    97 + ((pos + key) % 26)
+                }
+                _ => char,
+            });
+        }
+        // this is safe because non-utf8 bytes will never be pushed to ciphervec
+        unsafe { String::from_utf8_unchecked(ciphervec) }
+    }
+
+    pub fn decrypt(self, key: u8) -> String {
+        let mut plainvec: Vec<u8> = Vec::with_capacity(self.text.len());
+
+        for char in self.text.bytes() {
+            plainvec.push(match char {
+                65..=90 => {
+                    let pos = char % 65;
+                    65 + ((pos as i8 - key as i8).rem_euclid(26)) as u8
+                }
+                97..=122 => {
+                    let pos = char % 97;
+                    97 + ((pos as i8 - key as i8).rem_euclid(26)) as u8
+                }
+                _ => char,
+            });
+        }
+        // this is safe because non-utf8 bytes will never be pushed to plainvec
+        unsafe { String::from_utf8_unchecked(plainvec) }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_decrypt_basic() {
+        let input = String::from("Drsc sc k coxdoxmo");
+        let output = String::from("This is a sentence");
+
+        let message = Message::new(input, Mode::Decrypt);
+        let key: u8 = 10;
+
+        assert_eq!(message.decrypt(key), output);
+    }
+
+    #[test]
+    fn test_encrypt_basic() {
+        let input = String::from("Tests are important");
+        let output = String::from("Nymnm uly cgjilnuhn");
+
+        let message = Message::new(input, Mode::Encrypt);
+        let key: u8 = 20;
+
+        assert_eq!(message.encrypt(key), output);
+    }
+
+    #[test]
+    fn test_emoji_passthrough() {
+        let input = String::from("😀 😁 😂 🤣 😃 😄 😅 😆 😉 😊 😋 😎 😍");
+
+        let message = Message::new(input, Mode::Encrypt);
+        let key: u8 = 15;
+
+        assert_eq!(&message.encrypt(key.clone()), &input);
+        assert_eq!(&message.decrypt(key), &input);
+    }
+}
--- src/threads.rs
+++ src/threads.rs
@@ -2,44 +2,39 @@ use std::io;
 use std::io::Write;
 use std::thread;
 
-#[path = "crypt.rs"]
-mod crypt;
+#[path = "message.rs"]
+mod message;
+pub use message::{Mode, Message};
 
-pub fn run_jobs(message: String, mode: String, key: u8, threads: usize) {
-    let size: usize = message.len() / threads;
-    let jobs;
+pub fn run_jobs(message: Message, key: u8, threads: usize) {
+    // index of last char in String
+    let length = message.text.len();
+
+    let size: usize = length / threads;
 
     // if the length of the message is less than the thread count, do the job on one thread only
-    if message.len() < threads {
-        jobs = 0;
-    } else {
-        jobs = threads;
-    }
+    let jobs = if length < threads { 0 } else { threads };
 
     let main_thread_result;
     let mut children = Vec::with_capacity(jobs);
 
-    // index of last char in String
-    let length = message.len();
+    match message.mode {
+        Mode::Encrypt => {
+            for index in 0..jobs {
+                let chunk = Message::new(String::from(&message.text[index * size..(index + 1) * size]));
+                children.push(thread::spawn(move || chunk.encrypt(key)));
+            }
+            let last = Message::new(String::from(&message.text[size * jobs..length]));
+            main_thread_result = last.encrypt(key);
+        }
 
-    match &mode[..] {
-        "encrypt" => {
+        Mode::Decrypt => {
             for index in 0..jobs {
-                let chunk = String::from(&message[index * size..(index + 1) * size]);
-                children.push(thread::spawn(move || crypt::encrypt(chunk, key)));
+                let chunk = Message::new(String::from(&message.text[index * size..(index + 1) * size]));
+                children.push(thread::spawn(move || chunk.decrypt(key)));
             }
-            main_thread_result = crypt::encrypt(String::from(&message[size * jobs..length]), key);
-        }
-        "decrypt" => {
-            for index in 0..jobs {
-                let chunk = String::from(&message[index * size..(index + 1) * size]);
-                children.push(thread::spawn(move || crypt::decrypt(chunk, key)));
-            }
-            main_thread_result = crypt::decrypt(String::from(&message[size * jobs..length]), key);
-        }
-        _ => {
-            eprintln!("Mode must be 'encrypt' or 'decrypt'");
-            std::process::exit(1);
+            let last = Message::new(String::from(&message.text[size * jobs..length]));
+            main_thread_result = last.decrypt(key);
         }
     }
 
