0a9bad984212b406fb36ebe73e020e50ed763e37
abyss <derribado@tutanota.com>
2020-02-07 20:48:19 -0800

    Fixed mistakingly adding data from an empty buffer on files smaller than the combined size of all buffers

--- src/jobs/file.rs
+++ src/jobs/file.rs
@@ -1,3 +1,4 @@
+use super::buffer;
 use csr::Caesar;
 use num_cpus as cpus;
 use rayon::prelude::*;
@@ -6,52 +7,51 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader, Write};
 
 pub fn run(decrypt: bool, key: u8, path: &str) -> Result<(), Box<dyn Error>> {
-    let translate = if decrypt {
+    let translate: fn(Caesar, &mut [u8]) = if decrypt {
         Caesar::decrypt_bytes
     } else {
         Caesar::encrypt_bytes
     };
 
     let caesar = Caesar::new(key);
-    let cpus = cpus::get();
 
     let f = File::open(path)?;
     let mut reader = BufReader::new(f);
-    let mut bufs: Vec<[u8; 8192]> = buffers(cpus);
+    let mut bufs: Vec<[u8; 8192]> = buffer::get();
 
     let stdout = io::stdout();
     let mut handle = stdout.lock();
 
     'outer: loop {
+        let mut filled = cpus::get();
+        let mut bytes = 0;
         for (i, buf) in (&mut bufs).iter_mut().enumerate() {
             let size = reader.read(buf)?;
 
             if size == 0 {
                 match i {
                     0 => break 'outer,
-                    _ => break,
+                    // the amount of cpus filled with data
+                    _ => {
+                        filled = i;
+                        break;
+                    }
                 }
+            } else {
+                bytes = size;
             }
         }
 
         bufs.par_iter_mut().for_each(|buf| translate(caesar, buf));
 
-        for buf in &bufs {
-            let message = std::str::from_utf8(buf)?;
+        // operating on each filled buffer
+        for buf in 0..filled - 1 {
+            let message = std::str::from_utf8(&bufs[buf])?;
             write!(&mut handle, "{}", message)?;
         }
+        let last = std::str::from_utf8(&bufs[filled - 1][0..bytes])?;
+        write!(&mut handle, "{}", last)?;
     }
 
     Ok(())
-}
-
-fn buffers(cpus: usize) -> Vec<[u8; 8192]> {
-    let mut bufs: Vec<[u8; 8192]> = Vec::with_capacity(cpus);
-
-    for _ in 0..=cpus {
-        let buf: [u8; 8192] = [0; 8192];
-        bufs.push(buf);
-    }
-
-    bufs
 }
