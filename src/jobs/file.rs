1422b0bfc13ce2911ed3f60cf76b58beabab88bc
abyss <derribado@tutanota.com>
2020-02-07 21:56:40 -0800

    Refactored a bit to reduce copied code

--- src/jobs/file.rs
+++ src/jobs/file.rs
@@ -1,57 +1,14 @@
 use super::buffers;
 use csr::Caesar;
-use num_cpus as cpus;
-use rayon::prelude::*;
 use std::error::Error;
+
 use std::fs::File;
-use std::io::{self, prelude::*, BufReader, Write};
+pub use std::io::{self, prelude::*, BufReader};
 
 pub fn run(decrypt: bool, caesar: Caesar, path: &str) -> Result<(), Box<dyn Error>> {
-    let translate: fn(Caesar, &mut [u8]) = if decrypt {
-        Caesar::decrypt_bytes
-    } else {
-        Caesar::encrypt_bytes
-    };
-
-    let cpus = cpus::get();
-
     let f = File::open(path)?;
     let mut reader = BufReader::new(f);
-    let mut bufs = buffers::get(cpus);
-
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
-
-    'outer: loop {
-        let mut filled = cpus;
-        let mut bytes = 0;
-        for (i, buf) in (&mut bufs).iter_mut().enumerate() {
-            let size = reader.read(buf)?;
-
-            if size == 0 {
-                match i {
-                    0 => break 'outer,
-                    // the amount of cpus filled with data
-                    _ => {
-                        filled = i;
-                        break;
-                    }
-                }
-            } else {
-                bytes = size;
-            }
-        }
-
-        bufs.par_iter_mut().for_each(|buf| translate(caesar, buf));
-
-        // operating on each filled buffer
-        for buf in 0..filled - 1 {
-            let message = std::str::from_utf8(&bufs[buf])?;
-            write!(&mut handle, "{}", message)?;
-        }
-        let last = std::str::from_utf8(&bufs[filled - 1][0..bytes])?;
-        write!(&mut handle, "{}", last)?;
-    }
+    buffers::run(decrypt, caesar, &mut reader)?;
 
     Ok(())
 }
